574b7119e86addbd279fdf060dfcbd5776322d4a
Mevini Silva <silvamevini19@gmail.com>
2025-11-20 23:07:47 +0530

    randome number

---
+++ random.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+
+public class random {
+
+    public static void main(String[] args) {
+        int c;
+        Random t = new Random();
+        for(c=1;c<=10;c++){
+            System.out.println(t.nextInt(100));
+        }
+    }
+}
